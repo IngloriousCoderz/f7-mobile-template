6b676b4b6ba10902c24d25a2b7dc51555d88e55a
IceOnFire <antony.mistretta@gmail.com>
2017-10-29 09:42:39 +0100

    Add some packages and change config

--- meta.js
+++ meta.js
@@ -1,25 +1,35 @@
 module.exports = {
   helpers: {
-    raw: function(options) {
-      return options.fn(this)
-    }
+    raw: options => options.fn(this)
   },
   prompts: {
     name: {
-      'type': 'string',
-      'required': true,
-      'message': 'Project name'
+      type: 'string',
+      required: true,
+      message: 'Project name'
     },
     description: {
-      'type': 'string',
-      'required': false,
-      'message': 'Project description',
-      'default': 'Nuxt.js project'
-    },
-    author: {
-      'type': 'string',
-      'message': 'Author'
-    },
+      type: 'string',
+      required: false,
+      message: 'Project description',
+      default: 'Nuxt.js project'
+    }
+    // NOTE: no need for this, I precompiled it
+    // author: {
+    //   'type': 'string',
+    //   'message': 'Author'
+    // },
   },
-  completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
-};
+  completeMessage: `{{#inPlace}}
+  To get started:
+
+    npm install # Or yarn
+    npm run dev
+  {{else}}
+  To get started:
+
+    cd {{destDirName}}
+    npm install # Or yarn
+    npm run dev
+  {{/inPlace}}`
+}
